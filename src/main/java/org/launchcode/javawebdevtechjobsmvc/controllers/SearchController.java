fc3d8b9ff4531b83f87fc496e6181d8647a967a3
Liliana Gutierrez-Hernandez <lgutierrezh79@gmail.com>
2020-08-05 21:51:56 -0400

    assignment3 completed

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -12,6 +12,11 @@ import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.co
 
 /**
  * Created by LaunchCode
+ * columnChoices.put("all", "All");
+ *         columnChoices.put("employer", "Employer");
+ *         columnChoices.put("location", "Location");
+ *         columnChoices.put("positionType", "Position Type");
+ *         columnChoices.put("coreCompetency", "Skill");
  */
 @Controller
 @RequestMapping("search")
@@ -20,9 +25,23 @@ public class SearchController {
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
+        model.addAttribute("searchType","all");
         return "search";
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-
+    @PostMapping
+    public String displaySearchResults(@RequestParam String searchType, @RequestParam String searchTerm, Model model){
+        ArrayList<Job> jobs;
+        if (searchType == "all"){
+            jobs = JobData.findByValue(searchTerm);
+        }else {
+            jobs = JobData.findByColumnAndValue(searchType,searchTerm);
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs",jobs);
+        model.addAttribute("searchType",searchType);
+        model.addAttribute("searchTerm",searchTerm);
+        return "search";
+    }
 }
